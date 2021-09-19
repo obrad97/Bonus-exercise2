707bdcc54368916d81a9c28c5f6f4bd4a306c187
BuildTools <unconfigured@null.spigotmc.org>
2021-09-19 17:00:36 +0200

    finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,21 @@ questions.forEach((question) => {
         const activeQuestion = document.querySelector(".question.question-active");
         if (activeQuestion && activeQuestion !== question) {
             activeQuestion.classList.toggle("question-active")
+            activeQuestion.style.fontWeight = 'normal';
+            activeQuestion.style.color = '#4B4C5F';
             activeQuestion.nextElementSibling.style.maxHeight = '0px';
             activeQuestion.nextElementSibling.style.marginBottom = '0px';
         }
         const height = question.nextElementSibling.scrollHeight;
         question.classList.toggle('question-active');
         if (question.classList.contains('question-active')) {
+            question.style.fontWeight = 'bold';
+            question.style.color = '#1E1F36';
             question.nextElementSibling.style.maxHeight = `${height}px`;
             question.nextElementSibling.style.marginBottom = '16px';
         }else {
+            question.style.fontWeight = 'normal';
+            question.style.color = '#4B4C5F';
             question.nextElementSibling.style.maxHeight = '0px';
             question.nextElementSibling.style.marginBottom = '0px';
         }
